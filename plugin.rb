39b7f0022591939ec09a39426eb773237c5ade37
Robert <35533304+merefield@users.noreply.github.com>
2023-09-09 07:36:49 +0100

    Update plugin.rb
    Added added guidance in code, url

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,14 @@
 # name: discourse-isolation
 # authors: gerhard, merefield
 # about: Enables isolation mode after restoring backup.
-# version: 0.2
+# version: 0.3
+# url: https://github.com/merefield/discourse-isolation
 
 after_initialize do
-  on(:site_settings_restored) do
-    SiteSetting.disable_emails = "yes"
-    SiteSetting.login_required = true
-    SiteSetting.pop3_polling_enabled = false
-    WebHook.update_all(active: false)
+  on(:site_settings_restored) do # fork and configure how you see fit
+    SiteSetting.disable_emails = "yes" # prevent all outgoing email traffic
+    SiteSetting.login_required = true # block anon access so site is completely private
+    SiteSetting.pop3_polling_enabled = false # disable polling for incoming mail
+    WebHook.update_all(active: false) # prevent Discourse from triggering webhooks
   end
 end
